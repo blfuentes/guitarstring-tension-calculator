f44e28dc229c49ed02ef22b2af8fb913aa1f1b3f
blfuentes <insanespeech@hotmail.es>
2020-05-01 22:12:44 +0200

    Moved blazor project to new folder. Added Tests

---
+++ src/GSTC.Tests/TestBase.cs
@@ -0,0 +1,29 @@
+using GuitarStringTensionCalculator.Pages;
+using Microsoft.AspNetCore.Components.Testing;
+using System;
+using Xunit;
+
+namespace GSTC.Tests
+{
+    public class CounterTest
+    {
+        private TestHost host = new TestHost();
+
+        [Fact]
+        public void CountStartsAtZero()
+        {
+            var component = host.AddComponent<Counter>();
+            
+            Assert.Equal("Current count: 0", component.Find("p").InnerText);
+        }
+
+        [Fact]
+        public void CanIncrementCount()
+        {
+            var component = host.AddComponent<Counter>();
+
+            component.Find("button").Click();
+            Assert.Equal("Current count: 1", component.Find("p").InnerText);
+        }
+    }
+}
