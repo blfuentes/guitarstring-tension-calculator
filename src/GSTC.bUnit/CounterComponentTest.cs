99314dfa10d099e773fb44b276b68b4f47330ebf
blfuentes <insanespeech@hotmail.es>
2020-05-03 17:58:47 +0200

    Added bUnit project for unit testing

---
+++ src/GSTC.bUnit/CounterComponentTest.cs
@@ -0,0 +1,72 @@
+using System;
+using Xunit;
+using Bunit;
+using Bunit.Mocking.JSInterop;
+using GuitarStringTensionCalculator.Pages;
+using Shouldly;
+
+namespace GSTC.bUnit
+{
+    public class CounterComponentTest : ComponentTestFixture
+    {
+        [Fact]
+        public void InitialHtmlIsCorrect()
+        {
+            // Arrange - renders the Counter component
+            var cut = RenderComponent<Counter>();
+
+            // Assert
+            // Here we specify expected HTML from CUT.
+            var expectedHtml = @"<h1>Counter</h1>
+                                <p>Current count: 0</p>
+                                <button class=""btn-primary btn"">Click me</button>";
+
+            // Here we use the HTML diffing library to assert that the rendered HTML
+            // from CUT is semantically the same as the expected HTML string above.
+            cut.MarkupMatches(expectedHtml);
+        }
+
+        [Fact]
+        public void ClickingButtonIncreasesCountStrict()
+        {
+            // Arrange - renders the Counter component
+            var cut = RenderComponent<Counter>();
+
+            // Act
+            // Use a Find to query the rendered DOM tree and find the button element
+            // and trigger the @onclick event handler by calling Click
+            cut.Find("button").Click();
+
+            // Assert
+            // GetChangesSinceFirstRender returns list of differences since the first render,
+            // in which we assert that there should only be one change, a text change where
+            // the new value is provided to the ShouldHaveSingleTextChange assert method.
+            cut.GetChangesSinceFirstRender().ShouldHaveSingleTextChange("Current count: 1");
+
+            // Repeat the above steps to ensure that counter works for multiple clicks
+            cut.Find("button").Click();
+            cut.GetChangesSinceFirstRender().ShouldHaveSingleTextChange("Current count: 2");
+        }
+
+        [Fact]
+        public void ClickingButtonIncreasesCountTargeted()
+        {
+            // Arrange - renders the Counter component
+            var cut = RenderComponent<Counter>();
+
+            // Act
+            // Use a Find to query the rendered DOM tree and find the button element
+            // and trigger the @onclick event handler by calling Click
+            cut.Find("button").Click();
+
+            // Assert
+            // Use a Find to query the rendered DOM tree and find the paragraph element
+            // and assert that its text content is the expected (calling Trim first to remove insignificant whitespace)
+            cut.Find("p").TextContent.Trim().ShouldBe("Current count: 1");
+
+            // Repeat the above steps to ensure that counter works for multiple clicks
+            cut.Find("button").Click();
+            cut.Find("p").TextContent.Trim().ShouldBe("Current count: 2");
+        }
+    }
+}
---
+++ src/GSTC.bUnit/GuitarStringTest.cs
@@ -0,0 +1,33 @@
+﻿using Calculator.DLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GSTC.bUnit
+{
+    public class GuitarStringTest
+    {
+        [Fact]
+        public void GuitarString()
+        {
+            //d = 1 / L * √T * 1 / F * 1 /√ (π * ρ)
+            //double d = (1.0 / 0.59) * Math.Sqrt(35) * (1.0 / tmp_gut.Note.Frequency) * (1.0 / Math.Sqrt(Math.PI * tmp_gut.Material.Density));
+            //double t = Math.Pow(d * 0.59 * tmp_gut.Note.Frequency * Math.Sqrt(Math.PI * tmp_gut.Material.Density), 2);
+
+            GuitarString tmp_ssteel_e4 = new GuitarString("PL", "010", 25.5, (new FreqRepo()).GetMusicalNote("E", 4), new StringMaterial(MaterialType.STAINLESS_STEEL));
+            GuitarString tmp_ssteel_b3 = new GuitarString("PL", "014", 25.5, (new FreqRepo()).GetMusicalNote("B", 3), new StringMaterial(MaterialType.STAINLESS_STEEL));
+            GuitarString tmp_ssteel_g3 = new GuitarString("PL", "022", 25.5, (new FreqRepo()).GetMusicalNote("G", 3), new StringMaterial(MaterialType.STAINLESS_STEEL));
+            GuitarString tmp_ssteel_d3 = new GuitarString("NW", "030", 25.5, (new FreqRepo()).GetMusicalNote("D", 3), new StringMaterial(MaterialType.STAINLESS_STEEL));
+            GuitarString tmp_ssteel_a2 = new GuitarString("NW", "039", 25.5, (new FreqRepo()).GetMusicalNote("A", 2), new StringMaterial(MaterialType.STAINLESS_STEEL));
+            GuitarString tmp_ssteel_e2 = new GuitarString("NW", "046", 25.5, (new FreqRepo()).GetMusicalNote("E", 2), new StringMaterial(MaterialType.STAINLESS_STEEL));
+
+            Assert.Equal(16.2, Math.Round(tmp_ssteel_e4.StringTension_UW_LB, 1));
+            Assert.Equal(17.8, Math.Round(tmp_ssteel_b3.StringTension_UW_LB, 1));
+            Assert.Equal(27.7, Math.Round(tmp_ssteel_g3.StringTension_UW_LB, 1));
+            Assert.Equal(25.0, Math.Round(tmp_ssteel_d3.StringTension_UW_LB, 1));
+            Assert.Equal(22.8, Math.Round(tmp_ssteel_a2.StringTension_UW_LB, 1));
+            Assert.Equal(17.5, Math.Round(tmp_ssteel_e2.StringTension_UW_LB, 1));
+        }
+    }
+}
