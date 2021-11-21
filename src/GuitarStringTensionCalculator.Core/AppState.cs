bd0c1c3eaf48e261798fe5aef9e768c55174ed02
blfuentes <epiphonetodie@gmail.com>
2021-11-21 13:38:38 +0100

    Working on AppState

---
+++ src/GuitarStringTensionCalculator.Core/AppState.cs
@@ -0,0 +1,31 @@
+﻿namespace GuitarStringTensionCalculator.Core
+{
+    public class AppState
+    {
+        public int SelectedNumberOfStrings { get; private set; }
+        public double SelectedScaleLength { get; private set; }
+        public int SelectedTuning { get; private set; }
+
+        public event Action OnChange;
+
+        public void SetNumberOfStrings(int strings)
+        {
+            SelectedNumberOfStrings = strings;
+            NotifyStateChanged();
+        }
+
+        public void SetScaleLength(double scalelength)
+        {
+            SelectedScaleLength = scalelength;
+            NotifyStateChanged();
+        }
+
+        public void SetTuning(int tuning)
+        {
+            SelectedTuning = tuning;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
