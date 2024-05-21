43b2c5718fd3eda559c04b302ffed18ad0b009d1
blfuentes <blfuentes@mundo-r.com>
2024-05-21 08:54:48 +0200

    working on: fixing unit testing generator component

--- src/GSTC.bUnit/GeneratorComponentTest.cs
+++ src/GSTC.bUnit/GeneratorComponentTest.cs
@@ -3,20 +3,46 @@ using _pages = GuitarStringTensionCalculator.Components.Pages;
 using Xunit;
 using Shouldly;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using MudBlazor;
+using System.Linq;
 
 namespace GSTC.bUnit
 {
     public class GeneratorComponentTest : TestContext
     {
+        [Fact]
         public void InitialHtmlIsCorrect()
         {
+            Services.AddMudServices();
+            Services.AddMudBlazorKeyInterceptor();
+
+            JSInterop.SetupVoid("mudPopover.initialize", _ => true);
+            JSInterop.SetupVoid("mudPopover.connect", _ => true);
+            JSInterop.SetupVoid("mudKeyInterceptor.connect", _ => true);
+            JSInterop.SetupVoid("mudKeyInterceptor.updatekey", _ => true);
+
             // Arrange - renders the Generator component
-            var cut = RenderComponent<_pages.Generator>();
-            cut.Find("#_result_numberofstrings").GetAttribute("value").ShouldBe("0");
-            cut.Find("select").Change(new ChangeEventArgs() { Value = "6" });
-            cut.Find("#_result_numberofstrings").GetAttribute("value").ShouldBe("6");
-            cut.Find("select").Change(new ChangeEventArgs() { Value = "" });
-            cut.Find("#_result_numberofstrings").GetAttribute("value").ShouldBe("6");
+            var comp = RenderComponent<_pages.Generator>();
+            var select = comp.FindComponent<MudSelect<string>>();
+            //var menu = comp.Find("div.mud-popover");
+            var input = comp.Find("div.mud-input-control");
+            // check popover class
+            //menu.ClassList.ShouldContain("select-popover-class");
+            // check initial state
+            select.Instance.Value.ShouldBeNullOrEmpty();
+            input.Click();
+            comp.WaitForAssertion(() => comp.FindAll("div.mud-list-item").Count.ShouldBeGreaterThan(0));
+            var items = comp.FindAll("div.mud-list-item").ToArray();
+            items[1].Click();
+            select.Instance.Value.ShouldBe("7");
+
+            //cut.Find("#_result_numberofstrings").GetAttribute("value").ShouldBe("0");
+            //cut.Find("select").Change(new ChangeEventArgs() { Value = "6" });
+            //cut.Find("#_result_numberofstrings").GetAttribute("value").ShouldBe("6");
+            //cut.Find("select").Change(new ChangeEventArgs() { Value = "" });
+            //cut.Find("#_result_numberofstrings").GetAttribute("value").ShouldBe("6");
         }
     }
 }
